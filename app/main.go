605f6cbaae2707b3d6aa0c9febeb1b2fca8e63ad
Gonzalo Lepetic <lepeticg@gmail.com>
2026-07-22 12:46:40 +0200

    (task 3): basic REPL implementation

--- app/main.go
+++ app/main.go
@@ -10,18 +10,20 @@ import (
 var _ = fmt.Print
 
 func main() {
-	var userInput string
-	fmt.Print("$ ")
-	n, err := fmt.Scan(&userInput)
-	if err != nil {
-		fmt.Printf("Internal error when scanning user input: %s", err)
-		os.Exit(-1)
+	for {
+		var userInput string
+		fmt.Print("$ ")
+		n, err := fmt.Scan(&userInput)
+		if err != nil {
+			fmt.Printf("Internal error when scanning user input: %s", err)
+			os.Exit(-1)
+		}
+		var command string
+		if n > 1 {
+			command = strings.Split(userInput, " ")[0]
+		} else {
+			command = userInput
+		}
+		fmt.Printf("%s: command not found\r\n", command)
 	}
-	var command string
-	if n > 1 {
-		command = strings.Split(userInput, " ")[0]
-	} else {
-		command = userInput
-	}
-	fmt.Printf("%s: command not found\r\n", command)
 }
