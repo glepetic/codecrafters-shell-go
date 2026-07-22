a081bd183e9c501561106374bc970768cb70dbcd
Gonzalo Lepetic <lepeticg@gmail.com>
2026-07-22 12:08:18 +0200

    (task 2): handle command not found

--- app/main.go
+++ app/main.go
@@ -2,11 +2,26 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strings"
 )
 
 // Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
 var _ = fmt.Print
 
 func main() {
+	var userInput string
 	fmt.Print("$ ")
+	n, err := fmt.Scan(&userInput)
+	if err != nil {
+		fmt.Printf("Internal error when scanning user input: %s", err)
+		os.Exit(-1)
+	}
+	var command string
+	if n > 1 {
+		command = strings.Split(userInput, " ")[0]
+	} else {
+		command = userInput
+	}
+	fmt.Printf("%s: command not found\r\n", command)
 }
